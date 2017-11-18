b8f71f3877fd87c762465ba2c450921c165613f4
Yuanyuan Sheng <ysheng4@usfca.edu>
2017-11-17 21:45:21 -0800

    updating

--- Practice7Test.java
+++ Practice7Test.java
@@ -17,7 +17,7 @@ public class Practice7Test {
 		String val = "push";
 		boolean success = false;
 		
-		hashtable = new Hashtable();
+		Hashtable hashtable = new Hashtable();
 		
 		try {
 			hashtable.put(key, val);
@@ -36,10 +36,10 @@ public class Practice7Test {
 		String key = "American Terrorist";
 		boolean success = false;
 		
-		hashtable = new Hashtable();
+		Hashtable hashtable = new Hashtable();
 		
 		try {
-			if (! hashtable.contains(key)) {
+			if (! hashtable.containsKey(key)) {
 				if (hashtable.get(key).equals(null))
 					success = true;
 				else
@@ -59,7 +59,7 @@ public class Practice7Test {
 		String val = "I dream of you amid the flowers";
 		boolean success = false;
 		
-		hashtable = new Hashtable();
+		Hashtable hashtable = new Hashtable();
 		
 		try {
 			hashtable.put(key, val);
@@ -81,10 +81,10 @@ public class Practice7Test {
 		String key = "food for thought";
 		boolean success = false;
 		
-		hashtable = new Hashtable();
+		Hashtable hashtable = new Hashtable();
 		
 		try {
-			if (! hashtable.contains(key)) {
+			if (! hashtable.containsKey(key)) {
 				String shouldNotExist = hashtable.remove(key);
 				if (shouldNotExist != null || shouldNotExist.length() > 0)
 					success = false;
@@ -109,7 +109,7 @@ public class Practice7Test {
 		long start = 0;
 		long end = 0;
 		
-		hashtable = new Hashtable();
+		Hashtable hashtable = new Hashtable();
 		
 		try {
 		    reader = new BufferedReader(new FileReader(file));
